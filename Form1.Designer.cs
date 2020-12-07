53546b10f3c2673b57e2d48c208bdf50d494d391
tuanconbu <hieutk5@gmail.com>
2020-12-07 23:23:01 +0700

    done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lsbProcess = new System.Windows.Forms.ListBox();
+            this.btnKillProcess = new System.Windows.Forms.Button();
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.CPUText = new System.Windows.Forms.TextBox();
+            this.RAMText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lsbProcess);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(588, 338);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "All Process";
+            // 
+            // lsbProcess
+            // 
+            this.lsbProcess.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsbProcess.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbProcess.FormattingEnabled = true;
+            this.lsbProcess.Location = new System.Drawing.Point(3, 16);
+            this.lsbProcess.Name = "lsbProcess";
+            this.lsbProcess.Size = new System.Drawing.Size(582, 319);
+            this.lsbProcess.TabIndex = 0;
+            // 
+            // btnKillProcess
+            // 
+            this.btnKillProcess.Location = new System.Drawing.Point(506, 356);
+            this.btnKillProcess.Name = "btnKillProcess";
+            this.btnKillProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnKillProcess.TabIndex = 1;
+            this.btnKillProcess.Text = "End Task";
+            this.btnKillProcess.UseVisualStyleBackColor = true;
+            this.btnKillProcess.Click += new System.EventHandler(this.btnKillProcess_Click);
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(3, 356);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAll.TabIndex = 2;
+            this.btnGetAll.Text = "Get All";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(84, 356);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // CPUText
+            // 
+            this.CPUText.Location = new System.Drawing.Point(292, 358);
+            this.CPUText.Name = "CPUText";
+            this.CPUText.Size = new System.Drawing.Size(45, 20);
+            this.CPUText.TabIndex = 4;
+            // 
+            // RAMText
+            // 
+            this.RAMText.Location = new System.Drawing.Point(383, 358);
+            this.RAMText.Name = "RAMText";
+            this.RAMText.Size = new System.Drawing.Size(46, 20);
+            this.RAMText.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CPU:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "RAM:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "GB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(588, 396);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RAMText);
+            this.Controls.Add(this.CPUText);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnGetAll);
+            this.Controls.Add(this.btnKillProcess);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lsbProcess;
+        private System.Windows.Forms.Button btnKillProcess;
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox CPUText;
+        private System.Windows.Forms.TextBox RAMText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,86 @@ namespace ProcessManager
         public Form1()
         {
             InitializeComponent();
+        }
+        List<int> dsProcess = new List<int>();
+
+        protected System.Diagnostics.PerformanceCounter cpuCounter;
+        static double GetTotalMemoryInBytes()
+        {
+            return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+        }
+        private void btnGetAll_Click(object sender, EventArgs e)
+        {
+            DanhSachProcess();
+
+        }
+        private void DanhSachProcess()
+        {
+            dsProcess = new List<int>();
+            lsbProcess.Items.Clear();
+            Process[] p = Process.GetProcesses();
+            double ram = GetTotalMemoryInBytes() / (1024 * 1024); //MB
+            RAMText.Text = Math.Round(ram / 1024, 2).ToString();
+            cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            System.Threading.Thread.Sleep(1000);
+
+            //PerformanceCounter myAppCpu = new PerformanceCounter("Process", "% Processor Time", "_Total");
+            //double total_cpu = Convert.ToInt32(myAppCpu.NextValue());
+
+            int total_cpu = (int)cpuCounter.NextValue();
+            CPUText.Text = total_cpu.ToString();
+            foreach (Process ct in p)
+            {
+                try
+                {
+                    //CPU
+                    double cpu = new PerformanceCounter("Process", "% Processor Time", ct.ProcessName).NextValue();
+
+                    //RAM
+                    double memsize = 0;
+                    PerformanceCounter PC = new PerformanceCounter("Process", "Working Set - Private", ct.ProcessName, true);
+                    memsize = Convert.ToInt32(PC.NextValue()) / (1024 * 1024);
+                    PC.Close();
+                    lsbProcess.Items.Add("Name: " + ct.ProcessName + "\t" + " id: " + ct.Id + "\t" + "CPU:" + cpu + "\t" + "RAM usage: " + memsize + " MB");
+                    dsProcess.Add(ct.Id);
+                    if (memsize > 800)
+                    {
+                        DialogResult dialogResult = MessageBox.Show("Are you sure to close " + '"' + ct.ProcessName + '"' + "?", "Notice", MessageBoxButtons.YesNo);
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            Process.GetProcessById(ct.Id).Kill();
+                        }
+                    
+                        else if (dialogResult == DialogResult.No)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DanhSachProcess();
+        }
+
+        private void btnKillProcess_Click(object sender, EventArgs e)
+        {
+            if (lsbProcess.SelectedIndices.Count > 0)
+            {
+                int id = dsProcess[lsbProcess.SelectedIndex];
+                Process.GetProcessById(id).Kill();
+                DanhSachProcess();
+            }
         }
     }
 }
